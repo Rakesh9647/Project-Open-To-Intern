067bdf0bcc7d43877a0468d86eac0c89bae07583
Rakesh Kumar Sharma <rakeshsharma715078@gmail.com>
2022-03-23 12:06:26 +0530

    changes done

--- src/Controller/collegeController.js
+++ src/Controller/collegeController.js
@@ -3,12 +3,12 @@ const collegeModel = require('../Models/collegeModel')
 const College = async function (req, res) {
     try {
         const data = req.body
-        if (!data) return res.status(400).send({ status: false, message:"data is requried" })
+        if (Object.keys(data).length == 0) return res.status(400).send({ status:false ,message:'The request is not valid as the data are required.'})
         const saveData = await collegeModel.create(data)
         res.status(201).send({ status: true, message: saveData })
     }
     catch (err) {
-        res.status(500).send({ status: false, message: err });
+        res.status(500).send({ status: false, message: err.message });
     }
 }
 
--- src/Controller/internController.js
+++ src/Controller/internController.js
@@ -9,9 +9,9 @@ const Intern = async function (req, res) {
     try {
         let data = req.body
         let collegeId = req.body.collegeId
-        if (!data) return res.status(400).send({ status:false ,message:'The request is not valid as the data are required.'})
+        if (Object.keys(data).length == 0) return res.status(400).send({ status:false ,message:'The request is not valid as the data are required.'})
         let collegeDetail = await collegeModel.findById(collegeId)
-        if (!collegeDetail) return res.status(404).send({status:false, message:'The request is not valid as no intern is present with  given author id'})
+        if (!collegeDetail) return res.status(404).send({status:false, message:'The request is not valid as no intern is present with  given collage id'})
         let createNewIntern = await internModel.create(data)
         res.status(201).send({ status:true , data:createNewIntern })
     }
@@ -21,7 +21,6 @@ const Intern = async function (req, res) {
     }
 }
 
-
 const getDetail = async function (req, res) {
     try {
         let coll_name = req.query.collegeName
@@ -29,11 +28,11 @@ const getDetail = async function (req, res) {
             return res.status(400).send({status:false,  msg: "college name must be persent"})
         }
         let dataOne = await collegeModel.findOne({ name: coll_name })
+        if (!dataOne) {
+           return res.status(403).send({ status: false, message: "collage is not present" });
+        }
         let data = JSON.parse(JSON.stringify(dataOne))
         const C_id = dataOne._id
-        if (!dataOne) {
-           return res.status(403).send({ status: false, message: "The value is Invalid" });
-        }
         let interns = await internModel.find({ collegeId:C_id}).select({_id:true,name:true,email:true,mobile:true})
          data = {name:dataOne.name,fullName:dataOne.fullName,logoLink:dataOne.logoLink}
         data.interest = [...interns]
